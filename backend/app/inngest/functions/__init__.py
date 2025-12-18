afe549089d8dfc088dfab98998aaa80ea988e95d
styler1one <styler1one@gmail.com>
2025-12-18 18:47:23 +0100

    fix: register research-company-v2 in Inngest functions registry

--- backend/app/inngest/functions/__init__.py
+++ backend/app/inngest/functions/__init__.py
@@ -4,7 +4,7 @@ Inngest Functions Registry.
 This module exports all Inngest functions for registration with the serve endpoint.
 """
 
-from .research import research_company_fn
+from .research import research_company_fn, research_company_v2_fn
 from .preparation import preparation_meeting_fn
 from .followup import process_followup_audio_fn, process_followup_transcript_fn
 from .contacts import analyze_contact_fn
@@ -30,6 +30,7 @@ from .autopilot_execution import execute_proposal_fn
 # All functions to register with Inngest
 all_functions = [
     research_company_fn,
+    research_company_v2_fn,  # Exa-first research (V2)
     preparation_meeting_fn,
     process_followup_audio_fn,
     process_followup_transcript_fn,
@@ -58,6 +59,7 @@ all_functions = [
 __all__ = [
     "all_functions",
     "research_company_fn",
+    "research_company_v2_fn",
     "preparation_meeting_fn",
     "process_followup_audio_fn",
     "process_followup_transcript_fn",
